6b567be883942e197d31b8e09f0c7a8b4d55b015
SaunakBhattacharyya <53786831+SaunakBhattacharyya@users.noreply.github.com>
2019-09-19 18:50:17 +0530

    Add files via upload

---
+++ Coursera-Capstone (1).py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #this notebook will be mainly used for the capstone project.
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import Numpy as np
+
+
+# In[3]:
+
+
+print ("Hello Capstone Project Course!")
+
